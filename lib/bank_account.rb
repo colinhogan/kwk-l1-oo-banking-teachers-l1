87a7ce712d26128a1e733740f554d755b5b28e5b
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-13 12:55:08 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
 class BankAccount
-  # code here
+  
+  attr_accessor :balance, :status, :balance, :display_balance
+  attr_reader :name 
+  
+  def initialize(name, balance=1000, status="open")
+    @name = name
+    @balance = balance
+    @status = status
+  end 
+  
+  def deposit(amount=gets.strip.to_f)
+    @balance = @balance + amount   
+  end 
+  
+  def display_balance
+     return "Your Balance is $#{@balance}."
+  end 
+
+  def valid?
+    (@balance > 0) && (@status == "open")
+  end 
+
+  def close_account
+    @status = "closed"
+  end 
 end
+
+
+
+
+
+
+# colin_bank = BankAccount.new("Colin Hogan")
+
+
+# puts colin_bank.name
+# puts colin_bank.balance
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
-  # code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount=50) 
+    #code
+    @sender = sender
+    @receiver = receiver 
+    @status = "pending" 
+    @amount = amount
+    
+  end 
+  
+  def valid?
+    @sender.valid? && receiver.valid?
+  end 
+  
+  
 end
